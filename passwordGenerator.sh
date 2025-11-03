91446872c236d03e933e65e8c461d0d00583733e
MawiraVictor <victor.mawira@strathmore.edu>
2025-11-03 13:37:15 -0500

    enhancements

--- passwordGenerator.sh
+++ passwordGenerator.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+ 
+ #enhancements
+ # colors for output
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 
 #This is a simple password generator
 
 #1. prompt the user to enter the length of the password
-echo "This is simple password generator"
-echo "please enter the length of the password: "
+echo -e "${GREEN}This is simple password generator${NC}"
+echo -e "${BLUE}please enter the length of the password: "
 read PASS_LENGTH
 
 for p in $(seq 1 5);
